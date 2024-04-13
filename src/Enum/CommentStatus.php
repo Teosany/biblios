ed4162519d2c1464fb660effafeaad6a43ded53e
Teo <teosany@icloud.com>
2024-04-13 23:35:40 +0200

    add Entity Comment

---
+++ src/Enum/CommentStatus.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Enum;
+enum CommentStatus: string
+{
+    case Pending = 'pending';
+    case Published = 'published';
+    case Moderated = 'moderated';
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Pending => 'Pending',
+            self::Published => 'Published',
+            self::Moderated => 'Moderated',
+        };
+    }
+}
